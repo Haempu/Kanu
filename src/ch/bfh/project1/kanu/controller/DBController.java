5aa1bfe623875ef09813080a7519c20665eaff2b
Lukas <gestl1@bfh.ch>
2017-04-22 15:15:36 +0200

    start dbcontroller task

--- src/ch/bfh/project1/kanu/controller/DBController.java
+++ src/ch/bfh/project1/kanu/controller/DBController.java
@@ -1,8 +1,17 @@
 package ch.bfh.project1.kanu.controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import ch.bfh.project1.kanu.util.Row;
+
 /**
- * Die Klasse DBController ist zuständig, die Daten aus der Datenbank zur
- * Verfügung zu stellen und auf die Datenbank zu schreiben.
+ * Die Klasse DBController ist zuständig, die Daten aus der Datenbank zur Verfügung zu stellen und auf die Datenbank zu schreiben. 
  * 
  * @author Aebischer Patrik, Bösiger Elia, Gestach Lukas
  * @date 11.04.2017
@@ -11,5 +20,169 @@ package ch.bfh.project1.kanu.controller;
  */
 
 public class DBController {
+	private String dbHost;
+	private String db;
+	private String dbUser;
+	private String dbUserPassword;
 
+	private Connection connection;
+
+	private static DBController instance;
+	
+	private DBController() {
+		dbHost = "mysql22.webland.ch"; //TODO User und Passwort richtig setzen
+		db = "brave_res_tool";
+		dbUser = "brave_res_tool";
+		dbUserPassword = "SoED-purple1";
+
+		try {
+			connect();
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO: log exceptions
+		}
+	}
+	
+	public static synchronized DBController getInstance() {
+		if (instance == null) {
+			instance = new DBController();
+		}
+		return instance;
+	}
+	
+	/**
+	 * Connects to the database through jdbc and the underlining mysql-jdbc
+	 * driver.
+	 *
+	 * @throws ClassNotFoundException
+	 *             if the mysql-jdbc driver class cannot be located.
+	 * @throws SQLException
+	 *             if a database access error occurs.
+	 */
+	public void connect() throws ClassNotFoundException, SQLException {
+		// Load mysql-jdbc driver
+		Class.forName("com.mysql.jdbc.Driver");
+		// Connect to database
+		connection = DriverManager.getConnection("jdbc:mysql://" + dbHost + "/" + db + "", dbUser, dbUserPassword);
+	}
+	
+	/**
+	 * Disconnects from the database. It is strongly recommended that an
+	 * application explicitly commits or rolls back an active transaction prior
+	 * to calling the disconnect method.
+	 *
+	 * @throws SQLException
+	 *             if a database access error occurs.
+	 */
+	public void disconnect() throws SQLException {
+		connection.close();
+	}
+	
+	// --- UTIL METHODS ---
+		// Select and update methods are synchronized because the database isolation
+		// level may not be strong enough to lock these operations
+		// on the database itself.
+		// Can be changed if the database isolation level is high enough or the
+		// performance is too low .
+		// see ->
+		// https://en.wikipedia.org/wiki/Isolation_(database_systems)#Read_uncommitted
+		// and JAVA Monitoring and synchronizing.
+
+		/**
+		 * Performs an INSERT, UPDATE, or DELETE statement on the database.
+		 *
+		 * @param query
+		 *            - The query to be executed.
+		 * @return An ExecuteResult containing the result of the update operation.
+		 */
+		private synchronized ExecuteResult executeUpdate(String query) {
+			Statement updateStmt = null;
+			ExecuteResult result = new ExecuteResult();
+
+			try {
+				updateStmt = connection.createStatement();
+				updateStmt.executeUpdate(query);
+				ResultSet generatedKeys = updateStmt.getGeneratedKeys();
+				int i = 1;
+				while (generatedKeys.next()) {
+					result.addID(generatedKeys.getInt(i));
+					i++;
+				}
+				result.setSuccess(true);
+			} catch (SQLException e) {
+				// TODO: log exceptions
+				System.out.println("SQlException: " + e.getMessage());
+			} finally {
+				try {
+					// Release resources
+					if (updateStmt != null)
+						updateStmt.close();
+				} catch (SQLException e) {
+					// TODO: log exceptions
+					System.out.println("SQlException: " + e.getMessage());
+				}
+			}
+			return result;
+		}
+
+		/**
+		 * Executes a select statement on the database.
+		 *
+		 * @param query
+		 *            - The query to be executed.
+		 * @return A list containing the rows returned from the select statement or
+		 *         an empty list.
+		 */
+		private synchronized ArrayList<Row> executeSelect(String query) {
+			ArrayList<Row> results = new ArrayList<Row>();
+
+			Statement selectStmt = null;
+			ResultSet result = null;
+			try {
+				selectStmt = connection.createStatement();
+				result = selectStmt.executeQuery(query);
+				// Fetch rows
+				results = Row.formTable(result);
+			} catch (SQLException e) {
+				// TODO: log exceptions
+			} finally {
+				try {
+					// Release resources
+					if (selectStmt != null)
+						selectStmt.close();
+					if (result != null)
+						result.close();
+				} catch (SQLException e) {
+					// TODO: log exceptions
+				}
+			}
+			return results;
+		}
+
+		/**
+		 * Used to get the result of an execute statement.
+		 */
+		class ExecuteResult {
+			private boolean success = false;
+			private List<Integer> generatedIDs;
+
+			public ExecuteResult() {
+				generatedIDs = new ArrayList<Integer>();
+			}
+
+			public void setSuccess(boolean success) {
+				this.success = success;
+			}
+
+			public boolean isSuccess() {
+				return success;
+			}
+
+			public void addID(Integer id) {
+				generatedIDs.add(id);
+			}
+
+			public List<Integer> getGeneratedIDs() {
+				return generatedIDs;
+			}
+		}
 }
---
+++ src/ch/bfh/project1/kanu/util/Row.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2016 Berner Fachhochschule, Switzerland.
+ *
+ * Project Smart Reservation System.
+ *
+ * Distributable under GPL license. See terms of license at gnu.org.
+ */
+package ch.bfh.project1.kanu.util;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+/**
+ * Used to map SQL types into java types.
+ * TODO: Maybe a bit overkill for this application (use Spring Framework instead)
+ *
+ * @author Elias Schildknecht
+ * */
+public class Row {
+	private List<Entry<Object, Class<?>>> row;
+	private static Map<String, Class<?>> TYPE;
+
+	static {
+		TYPE = new HashMap<String,Class<?>>();
+
+		TYPE.put("INT", Integer.class);
+		TYPE.put("TINYINT", Boolean.class);
+		TYPE.put("VARCHAR", String.class);
+		TYPE.put("TIMESTAMP", Timestamp.class);
+	}
+
+	public Row(){
+		row = new ArrayList<Entry<Object, Class<?>>>();
+	}
+
+	/**
+	 * Adds data into the row with the associated java type.
+	 * */
+	private <T> void add(T data){
+		row.add(new AbstractMap.SimpleImmutableEntry<Object,Class<?>>(data, data.getClass()));
+	}
+
+	/**
+	 * Casts an sql-type into a java type and adds the data into the row.
+	 * */
+	private void add(Object data, String sqlType){
+		if(data != null){
+			add((Row.TYPE.get(sqlType)).cast(data));
+		}else{
+			row.add(new AbstractMap.SimpleImmutableEntry<Object,Class<?>>(null,null));
+		}
+	}
+
+	/**
+	 * Fetches the results of an sql-query into an ArrayList with rows.
+	 *
+	 * @param resultSet - The resultSet from the database operation
+	 * @return A table containing the rows from the sql-query or null if the query was empty.
+	 * */
+	public static ArrayList<Row> formTable(ResultSet resultSet){
+		ArrayList<Row> table = new ArrayList<Row>();
+
+		try {
+			ResultSetMetaData rsmd = resultSet.getMetaData();
+			int columnCount = rsmd.getColumnCount();
+
+			while(resultSet.next()){
+				Row currentRow = new Row();
+
+				for(int i=1; i<=columnCount; i++){
+					Object resultObject = resultSet.getObject(i);
+					String cloumnType = rsmd.getColumnTypeName(i);
+					currentRow.add(resultObject,cloumnType);
+				}
+				table.add(currentRow);
+			}
+		} catch (SQLException e) {
+			// TODO: log exceptions
+		}
+
+		return table;
+	}
+
+	public List<Entry<Object, Class<?>>> getRow() {
+		return row;
+	}
+}
