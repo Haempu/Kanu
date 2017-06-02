958683d805d12724992eae696110bf08af44ba60
Lukas <gestl1@bfh.ch>
2017-06-02 13:27:45 +0200

    ResultatComparator Anpassungen

--- src/main/java/ch/bfh/project1/kanu/util/ResultatComparator.java
+++ src/main/java/ch/bfh/project1/kanu/util/ResultatComparator.java
@@ -9,12 +9,8 @@ public class ResultatComparator implements Comparator<FahrerResultat> {
 	public int compare(FahrerResultat o, FahrerResultat q) {
 		//-1 wenn o kleiner als q --> wenn o schneller war als q
 		//+1 wenn o grösser als q --> wenn o langsamer war als q
-		Integer b = Math.min(o.getZeitErsterLauf(), o.getZeitZweiterLauf()); //TODO 0 ist als unendlich zu gewichten!
-		Integer a = Math.min(q.getZeitErsterLauf(), q.getZeitZweiterLauf());
-		if(a == 0)
-			return -1;
-		if(b == 0)
-			return 1;
+		Integer b = o.getZeitTotal();
+		Integer a = q.getZeitTotal();
 		if(a == b)
 			return 0;
 		if(a < b)
